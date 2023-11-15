6dadeccf78bb551066b19e903652458c87c0987d
lucyb <github@lucybridges.co.uk>
2023-11-15 21:02:38 +0000

    Pass in a JSON payload

--- api.py
+++ api.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class Reading(BaseModel):
+    nickname: str
+
 @app.post("/")
-def default():
+def default(reading: Reading):
     return {"msg": "Hello World"}
--- test_api.py
+++ test_api.py
@@ -4,7 +4,10 @@ from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
-def test_post_default():
-    response = client.post("/")
+def test_post_default_accepts_payload():
+    test_data = {
+        "nickname": "weather-test",
+    }
+
+    response = client.post("/", json=test_data)
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
