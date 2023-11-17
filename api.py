ae8142e70a8efa3559d12d7030b358cfba71f6ce
lucyb <github@lucybridges.co.uk>
2023-11-17 20:58:03 +0000

    Move readings into a specific model

--- api.py
+++ api.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from typing import Dict
 
 import uvicorn
 from fastapi import FastAPI
@@ -9,18 +8,28 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class Reading(BaseModel):
+    temperature: float
+    humidity: float
+    pressure: float
+    noise: float
+    pm1: float
+    pm2_5: float
+    pm10: float
+    voltage: float
+
+class Urban(BaseModel):
     nickname: str
     model: str
     uid: str
     timestamp: datetime
-    readings: Dict[str, float]
+    readings: Reading
 
 @app.get("/")
 def default():
     return
 
 @app.post("/urban")
-def urban(reading: Reading):
+def urban(urban: Urban):
     # Process data from the Enviro Urban
     # https://github.com/pimoroni/enviro/blob/main/documentation/boards/enviro-urban.md
     return
